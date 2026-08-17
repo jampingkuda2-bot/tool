b68b8434be37a53662bea0fca43a3e00ef2da9f6
AlexAlstr <jampingkuda2@gmail.com>
2026-08-17 08:47:41 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,21 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="id">
       <body>
-        {/* Monetag Onclick (Popunder) - aktif di seluruh halaman */}
-        <Script
-          id="monetag-onclick"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(s){s.dataset.zone='11591462',s.src='https://zovidree.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`,
-          }}
-        />
-
-        {/* Adsterra Popunder - aktif di seluruh halaman */}
-        <Script
-          id="adsterra-popunder"
-          src="https://pl30882979.effectivecpmnetwork.com/e4/c1/70/e4c1704dd04bcd3e4740fd29feaf2f9f.js"
-          strategy="afterInteractive"
-        />
+        {/* Iklan sekarang dipicu manual dari tombol "Tonton Iklan", bukan otomatis di seluruh halaman */}
 
         <header className="topbar">
           <a href="/" className="brand">
